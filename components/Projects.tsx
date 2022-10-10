45e06a165f1431b2df5ea62c9f4d028299747f05
Ahmad Alghali <a.alghali@primeboard.org>
2022-10-10 02:59:45 +0100

    init projects component

---
+++ components/Projects.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const projects = [];
+
+function Projects({}: Props) {
+  return (
+    <div>
+      <p className='text-3xl font-semibold text-center'>Projects</p>
+    </div>
+  );
+}
+
+export default Projects;
